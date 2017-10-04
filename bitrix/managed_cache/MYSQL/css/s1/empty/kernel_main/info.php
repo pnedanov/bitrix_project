1dfd98497175d7adce3569c09520f75cd59c35e6
pnedanov <nedanov@yandex.ru>
2017-10-05 01:04:19 +0400

    begin
    start project

---
+++ bitrix/managed_cache/MYSQL/css/s1/empty/kernel_main/info.php
@@ -0,0 +1,1 @@
+<? $arFilesInfo = array( 'FILES' => array("/bitrix/js/main/core/css/core.css" => "15070427822854","/bitrix/js/main/core/css/core_popup.css" => "150704278218079","/bitrix/js/main/core/css/core_date.css" => "15070427829657",), 'CUR_SEL_CNT' => '288', 'CUR_IE_CNT' => '0'); ?>
---
+++ bitrix/managed_cache/MYSQL/modules_css/26/262c58d032ce3e619e0ca3cc2830d98a.php
@@ -0,0 +1,7 @@
+<?
+if($INCLUDE_FROM_CACHE!='Y')return false;
+$datecreate = '001507043042';
+$dateexpire = '001543043042';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";s:60:"150704280715070428101507042815150704282515070428291507042844";}';
+return true;
+?>
